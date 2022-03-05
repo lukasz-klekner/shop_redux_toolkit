c232499cf6d41e2c80b355a1373988a6dadd857b
Łukasz Klekner <lukaszklekner@gmail.com>
2022-03-05 07:33:37 +0100

    adds uiSlice and uses it

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import { useSelector } from 'react-redux'
+
+import Cart from './components/Cart/Cart'
+import Layout from './components/Layout/Layout'
+import Products from './components/Shop/Products'
 
 function App() {
+  const { isCartVisible } = useSelector(({ ui }) => ui)
+
+  console.log(isCartVisible)
   return (
     <Layout>
-      <Cart />
+      {isCartVisible && <Cart />}
       <Products />
     </Layout>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,19 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from 'react-redux'
+
+import { toggle } from '../../redux/slices/uiSlice'
+import classes from './CartButton.module.css'
 
 const CartButton = (props) => {
+  const dispatch = useDispatch()
+
+  const toggleHandler = () => dispatch(toggle())
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
-  );
-};
+  )
+}
 
-export default CartButton;
+export default CartButton
